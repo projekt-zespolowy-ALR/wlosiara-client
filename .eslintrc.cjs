a1020b08b7648213961f97976b00ce7a05acd782
Rafał Majewski <goodheropl@gmail.com>
2022-10-28 23:54:54 +0200

    Fix formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,57 +2,46 @@ module.exports = {
 	root: true,
 	overrides: [
 		{
-			files: ['*.svelte'],
-			processor: 'svelte3/svelte3',
-			parser: '@typescript-eslint/parser',
+			files: ["*.svelte"],
+			processor: "svelte3/svelte3",
+			parser: "@typescript-eslint/parser",
 			parserOptions: {
-				project: './tsconfig.json',
+				project: "./tsconfig.json",
 				tsconfigRootDir: __dirname,
-				sourceType: 'module',
+				sourceType: "module",
 				ecmaVersion: 13,
 			},
-			extends: [
-				'eslint:recommended',
-				'plugin:@typescript-eslint/recommended',
-				'prettier'
-			],
-			plugins: ['svelte3', '@typescript-eslint'],
+			extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+			plugins: ["svelte3", "@typescript-eslint"],
 		},
 		{
-			files: ['*.ts'],
-			parser: '@typescript-eslint/parser',
+			files: ["*.ts"],
+			parser: "@typescript-eslint/parser",
 			parserOptions: {
-				project: './tsconfig.json',
+				project: "./tsconfig.json",
 				tsconfigRootDir: __dirname,
-				sourceType: 'module',
+				sourceType: "module",
 				ecmaVersion: 13,
 			},
-			extends: [
-				'eslint:recommended',
-				'plugin:@typescript-eslint/recommended',
-				'prettier'
-			],
-			plugins: ['@typescript-eslint'],
+			extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+			plugins: ["@typescript-eslint"],
 		},
 		{
-			files: ['*.js'],
+			files: ["*.js"],
 			parser: "espree",
 			parserOptions: {
 				ecmaVersion: 13,
-				sourceType: 'module',
+				sourceType: "module",
 			},
-			extends: [
-				'eslint:recommended',
-				'prettier',
-			],
+			extends: ["eslint:recommended", "prettier"],
 		},
 	],
 	settings: {
-		'svelte3/typescript': () => require('typescript')
+		"svelte3/typescript": () => require("typescript"),
 	},
 	env: {
 		browser: true,
 		es2022: true,
-		node: true
-	}
+		node: true,
+	},
 };
