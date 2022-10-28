2d6e63287b607254845f49c85d2d2c54f53fc627
Rafał Majewski <goodheropl@gmail.com>
2022-10-28 23:40:42 +0200

    Better eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,58 @@
 module.exports = {
 	root: true,
-	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			processor: 'svelte3/svelte3',
+			parser: '@typescript-eslint/parser',
+			parserOptions: {
+				project: './tsconfig.json',
+				tsconfigRootDir: __dirname,
+				sourceType: 'module',
+				ecmaVersion: 13,
+			},
+			extends: [
+				'eslint:recommended',
+				'plugin:@typescript-eslint/recommended',
+				'prettier'
+			],
+			plugins: ['svelte3', '@typescript-eslint'],
+		},
+		{
+			files: ['*.ts'],
+			parser: '@typescript-eslint/parser',
+			parserOptions: {
+				project: './tsconfig.json',
+				tsconfigRootDir: __dirname,
+				sourceType: 'module',
+				ecmaVersion: 13,
+			},
+			extends: [
+				'eslint:recommended',
+				'plugin:@typescript-eslint/recommended',
+				'prettier'
+			],
+			plugins: ['@typescript-eslint'],
+		},
+		{
+			files: ['*.js'],
+			parser: "espree",
+			parserOptions: {
+				ecmaVersion: 13,
+				sourceType: 'module',
+			},
+			extends: [
+				'eslint:recommended',
+				'prettier',
+			],
+		},
+	],
 	settings: {
 		'svelte3/typescript': () => require('typescript')
 	},
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020
-	},
 	env: {
 		browser: true,
-		es2017: true,
+		es2022: true,
 		node: true
 	}
 };
