d7a2e61cd9e4964a4b5883c994f028c1086833a4
Rafał Majewski <goodheropl@gmail.com>
2022-10-30 00:48:13 +0200

    Basic testing

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import {defineConfig} from "vitest/config";
+import {svelte} from "@sveltejs/vite-plugin-svelte";
+
+export default defineConfig({
+	plugins: [svelte({hot: !process.env.VITEST})],
+	test: {
+		include: ["./tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+		environment: "jsdom",
+	},
+});
