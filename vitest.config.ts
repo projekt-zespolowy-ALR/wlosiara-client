de8c64f69c4ff64386dfc3199ae7381b9c449571
Rafał Majewski <goodheropl@gmail.com>
2022-12-08 10:17:36 +0100

    Improve vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,13 @@ import {defineConfig} from "vitest/config";
 import {svelte} from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
-	plugins: [svelte({hot: !process.env.VITEST})],
+	plugins: [svelte({hot: !process.env["VITEST"]})],
 	test: {
-		include: ["./tests/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+		include: ["**/*.test.ts", "**/*.test.js", "**/*.test.cjs", "**/*.test.mjs"],
 		environment: "jsdom",
+		coverage: {
+			provider: "c8",
+			reporter: ["html", "text"],
+		},
 	},
 });
