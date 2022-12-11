013a451aa91e31fef7a8ab5051cb745e271d2722
Rafał Majewski <goodheropl@gmail.com>
2022-12-11 02:40:37 +0100

    Remove jest dom matchers

--- test/landing_page.unit.test.ts
+++ test/landing_page.unit.test.ts
@@ -1,12 +1,12 @@
 import {render, screen} from "@testing-library/svelte";
 import LandingPage from "../src/routes/+page.svelte";
-import matchers from "@testing-library/jest-dom/matchers";
 import {describe, expect, test} from "vitest";
-expect.extend(matchers);
 
 describe("Landing page", () => {
 	test("Quiz message", () => {
 		render(LandingPage);
-		expect(screen.getByText("Jakie są twoje włosy?")).toBeInTheDocument();
+		expect(() => {
+			screen.getByText("Jakie są twoje włosy?");
+		}).not.toThrow();
 	});
 });
