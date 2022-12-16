b6c1df54685c3e221111054db595b9fa18c38a57
Ludwika Malinowska <malinowska.L@wp.pl>
2022-12-16 15:24:53 +0100

    Skeleton Login/Register Page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Header from "$lib/ui/header/Header.svelte";
+	import Footer from "$lib/ui/footer/Footer.svelte";
+	import LoginPage from "$lib/features/login/LoginPage.svelte";
+</script>
+
+<Header />
+<LoginPage />
+<Footer />
