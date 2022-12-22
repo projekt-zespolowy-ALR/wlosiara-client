dbc042396c511a64f4dbb49153a8b19a3d6688dd
Ludwika Malinowska <malinowska.L@wp.pl>
2022-12-22 17:52:34 +0100

    Footer prepared, without working links

--- src/lib/ui/footer/Footer.svelte
+++ src/lib/ui/footer/Footer.svelte
@@ -1,11 +1,39 @@
 <script lang="ts"></script>
 
-<footer />
+<footer>
+	<div class="footer-links">
+		<a href="/">FAQ</a>
+		<a href="/">Jak to działa?</a>
+		<a href="/">O nas</a>
+		<a href="/">Kontakt</a>
+		<a href="/">Regulamin</a>
+	</div>
+
+	<p class="copyright">Copyright 2022. Wszystkie prawa zastrzeżone.</p>
+</footer>
 
 <style>
 	footer {
 		width: 100%;
-		height: 50px;
+		height: 80px;
 		background-color: #1f1f1f;
+		color: white;
+		font-size: 12px;
+		padding-top: 30px;
+	}
+
+	.footer-links {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-around;
+		width: 400px;
+	}
+
+	.footer-links a {
+		text-decoration: none;
+		color: white;
+	}
+	.copyright {
+		text-align: right;
 	}
 </style>
