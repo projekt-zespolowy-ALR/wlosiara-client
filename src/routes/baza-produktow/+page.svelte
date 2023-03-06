2c77bd3fb844cbf2fa39a5e134bad83314615928
Rafał Majewski <goodheropl@gmail.com>
2023-03-06 16:47:23 +0100

    ProductList route cleanup imports

--- src/routes/baza-produktow/+page.svelte
+++ src/routes/baza-produktow/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
 	import ProductList from "$lib/features/products/ProductList.svelte";
-	import Footer from "$lib/ui/footer/Footer.svelte";
-	import Header from "$lib/header/Header.svelte";
 </script>
 
 <ProductList />
