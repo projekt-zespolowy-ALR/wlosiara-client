a5eef9e0907b52b996c7e49d8e8b27dc7d223f08
Rafał Majewski <goodheropl@gmail.com>
2023-03-06 15:46:36 +0100

    LandingPage cleanup

--- src/lib/landing_page/LandingPage.svelte
+++ src/lib/landing_page/LandingPage.svelte
@@ -1,16 +1,11 @@
-<script lang="ts"></script>
-
 <div class="landing-page">
 	<h3>Witaj w serwisie włosiara.pl!</h3>
 </div>
 
-<style>
-	.landing-page {
-		background-color: var(--gray-8);
-		height: 550px;
+<style lang="scss">
+	/* .landing-page {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		margin-top: 10px;
-	}
+	} */
 </style>
