ec4b8967c3db2e959596ac2177ad1b68c3897b75
Rafał Majewski <Rafa Majewski>
2023-03-09 17:05:18 +0100

    Remove @ts-ignore comments in tests

--- test/quiz_page.unit.test.ts
+++ test/quiz_page.unit.test.ts
@@ -11,8 +11,7 @@ describe("Quiz page", () => {
 	});
 	test("Elements are clickable", () => {
 		render(QuizPage);
-		//@ts-ignore
-		fireEvent.click(screen.getAllByRole("button")[0]);
+		fireEvent.click(screen.getAllByRole("button")[0] as HTMLButtonElement);
 		expect(() => {
 			const question = document.querySelector(".question");
 			expect(question).not.toBeNull();
@@ -21,8 +20,7 @@ describe("Quiz page", () => {
 	test("Results are shown", () => {
 		render(QuizPage);
 		for (let i = 0; i < 21; i++) {
-			//@ts-ignore
-			fireEvent.click(screen.getAllByRole("button")[0]);
+			fireEvent.click(screen.getAllByRole("button")[0] as HTMLButtonElement);
 		}
 		expect(() => {
 			const result: string | null | undefined = document.querySelector("p")?.textContent;
