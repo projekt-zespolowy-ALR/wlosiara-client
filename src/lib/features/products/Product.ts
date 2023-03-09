7eb5546060b5702db8d71d7623ccf8f2e6f0dda0
Rafał Majewski <Rafa Majewski>
2023-03-09 16:34:57 +0100

    Product type

---
+++ src/lib/features/products/Product.ts
@@ -0,0 +1,41 @@
+type Product = Readonly<{
+	/**
+	 * The product's id in UUID format.
+	 * @example "a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11"
+	 */
+	id: string;
+
+	/**
+	 * The product's name if it's known, null otherwise.
+	 * @example "CLARESA TRUE GLUE"
+	 * @example null
+	 */
+	name: string | null;
+
+	/**
+	 * The product's slug. Use it in the links but use the `id` for API requests.
+	 * @example "claresa-true-glue"
+	 */
+	slug: string;
+
+	/**
+	 * The product's mass in killograms if it's known, null otherwise.
+	 * @example 0.4
+	 * @example null
+	 */
+	mass: number | null;
+
+	/**
+	 * The product's volume in liters if it's known, null otherwise.
+	 * @example 0.4
+	 * @example null
+	 */
+	volume: number | null;
+
+	/**
+	 * The product's categories.
+	 */
+	categories: readonly string[];
+}>;
+
+export default Product;
