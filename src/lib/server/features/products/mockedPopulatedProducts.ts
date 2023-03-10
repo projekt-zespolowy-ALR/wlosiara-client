56fd189885221bc707454233067a5ea309fd2320
Rafał Majewski <Rafa Majewski>
2023-03-10 01:59:52 +0100

    mockedPopulatedProducts

---
+++ src/lib/server/features/products/mockedPopulatedProducts.ts
@@ -0,0 +1,5 @@
+import type PopulatedProduct from "$lib/features/products/PopulatedProduct.js";
+
+const mockedPopulatedProducts: readonly PopulatedProduct[] = [];
+
+export default mockedPopulatedProducts;
