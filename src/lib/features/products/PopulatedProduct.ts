bc0ca9c32f6f057ba11dfbd5bd351ca5511512f0
Rafał Majewski <Rafa Majewski>
2023-03-10 01:38:55 +0100

    PopulatedProductInDataSource

--- src/lib/features/products/PopulatedProduct.ts
+++ src/lib/features/products/PopulatedProduct.ts
@@ -1,4 +1,4 @@
-import type DataSource from "$lib/features/data_sources/DataSource.js";
+import type PopulatedProductInDataSource from "./PopulatedProductInDataSource.js";
 import type ProductCategory from "./ProductCategory.js";
 
 type PopulatedProduct = {
@@ -43,27 +43,7 @@ type PopulatedProduct = {
 	/**
 	 * Information about the product's availability in the data sources.
 	 */
-	readonly inDataSources: readonly {
-		readonly dataSource: DataSource;
-		/**
-		 * The URL of the product's page in the data source.
-		 * @example "https://www.hebe.pl/claresa-zel-do-laminacji-brwi-8-g-000000000000407817.html"
-		 */
-		readonly referenceUrl: string | null;
-		/**
-		 * The URL of the product's image in the data source.
-		 * May not always work due to CORS.
-		 */
-		readonly imageUrl: string | null;
-		/**
-		 * The product's price in PLN in the data source if it's known, null otherwise.
-		 * @example 9.99
-		 * @example null
-		 */
-		readonly price: number | null;
-
-		readonly description: string | null;
-	}[];
+	readonly inDataSources: readonly Omit<PopulatedProductInDataSource, "product">[];
 };
 
 export default PopulatedProduct;
