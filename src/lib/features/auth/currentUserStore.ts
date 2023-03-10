869f1caac43d4734dbbcb548a82f1615b871253b
Rafał Majewski <Rafa Majewski>
2023-03-10 02:29:25 +0100

    Comment out UserCredentials as it's for future use

--- src/lib/features/auth/currentUserStore.ts
+++ src/lib/features/auth/currentUserStore.ts
@@ -1,11 +1,11 @@
 import type User from "$lib/features/users/User.js";
 import {writable} from "svelte/store";
-import type UserCredentials from "./UserCredentials.js";
+// import type UserCredentials from "./UserCredentials.js";
 
 function createCurrentUserStore() {
 	const {subscribe, set} = writable<User | null>(null);
 
-	const login = async (credentials: UserCredentials): Promise<void> => {
+	const login = async (/*credentials: UserCredentials*/): Promise<void> => {
 		set({
 			id: "mock-user-id",
 			username: "mockuser",
