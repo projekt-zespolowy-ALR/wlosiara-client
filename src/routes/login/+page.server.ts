07c4700920c72810066c87429325e37425bb9f79
Rafał Majewski <goodheropl@gmail.com>
2023-04-23 16:48:31 +0200

    Generate mocked sessions

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,7 +1,12 @@
 import type {Actions} from "./$types.js";
-
+import userIdBySessionToken from "$lib/server/features/auth/userIdBySessionToken.js";
+import mockedUsers from "$lib/server/features/users/mockedUsers.js";
+import type User from "$lib/features/users/types/User.js";
 export const actions: Actions = {
 	default: async ({cookies}) => {
-		cookies.set("sessionToken", "1234567890");
+		const user = mockedUsers[Math.floor(Math.random() * mockedUsers.length)] as User;
+		const sessionToken = Math.random().toString(36).slice(2);
+		userIdBySessionToken.set(sessionToken, user.id);
+		cookies.set("sessionToken", sessionToken);
 	},
 };
