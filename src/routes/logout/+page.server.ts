a3d1eabd9925193266d79187e8fbd614ae13e7db
Rafał Majewski <goodheropl@gmail.com>
2023-04-23 17:10:00 +0200

    Logout server action

---
+++ src/routes/logout/+page.server.ts
@@ -0,0 +1,11 @@
+import type {Actions} from "./$types.js";
+import userIdBySessionToken from "$lib/server/features/auth/userIdBySessionToken.js";
+
+export const actions: Actions = {
+	default: async ({cookies}) => {
+		const sessionToken = cookies.get("sessionToken");
+		if (sessionToken === undefined) return;
+		userIdBySessionToken.delete(sessionToken);
+		cookies.delete("sessionToken");
+	},
+};
