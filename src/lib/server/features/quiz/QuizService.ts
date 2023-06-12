4f683a6eb5842f86d90804c8232fac2a009504ff
Rafał Majewski <goodheropl@gmail.com>
2023-06-12 16:10:44 +0200

    getQuizById

--- src/lib/server/features/quiz/QuizService.ts
+++ src/lib/server/features/quiz/QuizService.ts
@@ -1,31 +1,45 @@
-import type {Page} from "$lib/server/utils/Page.js";
 import type {QuizApiClient} from "$lib/server/features/quiz/quiz_api_client/QuizApiClient.js";
-import type {Question} from "$lib/features/quiz/types/Question.js";
-import type {Answer} from "$lib/features/quiz/types/Answer.js";
-import type {AnswerToQuestion} from "$lib/features/quiz/types/AnswerToQuestion.js";
-import type {HairType} from "$lib/features/quiz/types/HairType.js";
-import type {QuizInApi } from "./quiz_api_client/types/QuizInApi.js";
+import type {Quiz} from "$lib/features/quiz/types/Quiz.js";
+import type {QuizQuestion} from "$lib/features/quiz/types/QuizQuestion.js";
+import type {QuizQuestionAnswer} from "$lib/features/quiz/types/QuizQuestionAnswer.js";
 
 export class QuizService {
-	private readonly quizsApiClient: QuizApiClient;
+	private readonly quizzesApiClient: QuizApiClient;
 
-	public constructor({quizsApiClient}: {quizsApiClient: QuizApiClient}) {
-		this.quizsApiClient = quizsApiClient;
+	public constructor({quizzesApiClient}: {quizzesApiClient: QuizApiClient}) {
+		this.quizzesApiClient = quizzesApiClient;
 	}
 
-    // private async populateQuizInApi(quizInApi: QuizInApi): Promise<Question[]> {
-    //     const questions = 
-    // }
-	// public async getQuizPage(): Promise<Page<Question>> {
-	// 	const quizPageInApi = await this.quizsApiClient.fetchQuizPage();
-	// 	return {
-	// 		...quizPageInApi,
-	// 		items: quizPageInApi.items,
-	// 	};
-	// }
-
-	// public async getQuizById(targetProductId: string): Promise<Product> {
-	// 	const quizInApi = await this.quizsApiClient.fetchProductById(targetProductId);
-	// 	return quiz;
-	// }
+	public async getQuizById(targetQuizId: string): Promise<
+		Omit<Quiz, "questions"> & {
+			questions: (Omit<QuizQuestion, "quiz" | "answers"> & {
+				answers: Omit<QuizQuestionAnswer, "question">[];
+			})[];
+		}
+	> {
+		const [
+			quizInApi,
+			{
+				meta: {totalItemsCount: totalQuestionsCount},
+			},
+		] = await Promise.all([
+			this.quizzesApiClient.fetchQuizById(targetQuizId),
+			this.quizzesApiClient.fetchQuizQuestionsPage(targetQuizId, {skip: 0, take: 0}),
+		]);
+		const questionsPage = await this.quizzesApiClient.fetchQuizQuestionsPage(targetQuizId, {
+			skip: 0,
+			take: totalQuestionsCount,
+		});
+		const questions = await Promise.all(
+			questionsPage.items.map(async (question) => ({
+				...question,
+				answers: await this.quizzesApiClient.fetchQuizQuestionAnswers(targetQuizId, question.id),
+			}))
+		);
+		const quiz = {
+			...quizInApi,
+			questions,
+		} as const;
+		return quiz;
+	}
 }
