6e0fadb0da5f83baf97f9a36464e3899973b79aa
Ludwika Malinowska <malinowska.L@wp.pl>
2023-06-13 22:03:00 +0200

    Login - work in progress

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,3 +1,26 @@
 import type {DeepReadonly} from "ts-essentials";
 import type {Actions} from "./$types.js";
-export const actions: DeepReadonly<Actions> = {} as const;
+import {invalidateAll} from "$app/navigation";
+import { authService } from "$lib/server/instances/authService.js";
+
+
+export const actions: DeepReadonly<Actions> = {
+    default: async (event) => {
+        // console.log("hello");
+		const data = await event.request.formData();
+        console.log(data);
+        const email = data.get('email')?.toString() ?? "";
+        const password = data.get('password')?.toString() ?? "";
+        
+        const sessid = await authService.login({email, password});
+        console.log("ssid", sessid);
+        event.cookies.set('sessionid', sessid.token);
+
+		// await fetch(formEl.action, {
+		// 	method: "POST",
+		// 	body: "",
+		// });
+
+		// await invalidateAll();
+    }
+} as const;
--- src/routes/logout/+page.server.ts
+++ src/routes/logout/+page.server.ts
@@ -1,3 +1,19 @@
 import type {DeepReadonly} from "ts-essentials";
 import type {Actions} from "./$types.js";
-export const actions: DeepReadonly<Actions> = {} as const;
+import {invalidateAll} from "$app/navigation";
+import { authService } from "$lib/server/instances/authService.js";
+
+export const actions: DeepReadonly<Actions> = {
+    default: async (event) => {
+		// const formEl = event.target as HTMLFormElement;
+		// await fetch(formEl.action, {
+		// 	method: "POST",
+		// 	body: "",
+		// });
+
+        event.cookies.delete('sessionid');
+        // event.locals.user = null;
+
+		await invalidateAll();
+    }
+} as const;
