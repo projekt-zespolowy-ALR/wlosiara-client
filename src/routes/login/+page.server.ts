79d4f21b58885a2308742d8e49917feac77ce208
Rafał Majewski <goodheropl@gmail.com>
2023-06-20 20:31:07 +0200

    Fix formatting

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,20 +1,20 @@
 import type {DeepReadonly} from "ts-essentials";
 import type {Actions} from "./$types.js";
 import {invalidateAll} from "$app/navigation";
-import { authService } from "$lib/server/instances/authService.js";
+import {authService} from "$lib/server/instances/authService.js";
 
 export const actions: DeepReadonly<Actions> = {
-    default: async (event) => {
-        // console.log("hello");
+	default: async (event) => {
+		// console.log("hello");
 		const data = await event.request.formData();
-        // console.log(data);
-        const email = data.get('email')?.toString() ?? "";
-        const password = data.get('password')?.toString() ?? "";
-        
-        const sessid = await authService.login({email, password});
-        // console.log("ssid", sessid);
-        event.cookies.set('session_token', sessid.token);
+		// console.log(data);
+		const email = data.get("email")?.toString() ?? "";
+		const password = data.get("password")?.toString() ?? "";
 
-        return !!sessid.token;
-    }
+		const sessid = await authService.login({email, password});
+		// console.log("ssid", sessid);
+		event.cookies.set("session_token", sessid.token);
+
+		return !!sessid.token;
+	},
 } as const;
--- src/routes/logout/+page.server.ts
+++ src/routes/logout/+page.server.ts
@@ -2,7 +2,7 @@ import type {DeepReadonly} from "ts-essentials";
 import type {Actions} from "./$types.js";
 
 export const actions: DeepReadonly<Actions> = {
-    default: async (event) => {
-        event.cookies.delete('session_token');
-    }
+	default: async (event) => {
+		event.cookies.delete("session_token");
+	},
 } as const;
