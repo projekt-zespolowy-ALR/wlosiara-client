9f1450a348388a8771ca0aa41a217ee21f95c1f2
Ludwika Malinowska <malinowska.L@wp.pl>
2023-06-21 20:23:33 +0200

    News panel

---
+++ src/routes/aktualnosci/+page.server.ts
@@ -0,0 +1,17 @@
+import type {BlogEntry} from "$lib/features/blog/types/BlogEntry.js";
+import type {DeepReadonly} from "ts-essentials";
+
+import type {PageServerLoad} from "./$types.js";
+
+import type {Page} from "$lib/server/utils/Page.js";
+
+export const load: PageServerLoad = async () => {
+	const {blogService} = await import("$lib/server/instances/blogService.js");
+	const blogEntriesPage: DeepReadonly<Page<BlogEntry>> = await blogService.getPostsPage({
+		number: 1,
+		size: 10,
+	});
+	return {
+		blogEntriesPage: blogEntriesPage,
+	} as const;
+};
