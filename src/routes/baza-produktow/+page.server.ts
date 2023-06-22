aaa64f5d3de58e8fd7ac437d15e58149bc53ec84
Rafał Majewski <goodheropl@gmail.com>
2023-06-22 14:32:21 +0200

    Fix ESLint

--- src/routes/baza-produktow/+page.server.ts
+++ src/routes/baza-produktow/+page.server.ts
@@ -4,7 +4,7 @@ import type {Page} from "$lib/server/utils/Page.js";
 import type {Product} from "$lib/features/products/types/Product.js";
 import type {PagingOptions} from "$lib/utils/PagingOptions.js";
 
-export const load: PageServerLoad = async ({url, cookies, parent}) => {
+export const load: PageServerLoad = async ({url, parent}) => {
 	const [{productsService}, {currentUser}] = await Promise.all([
 		import("$lib/server/instances/productsService.js"),
 		parent(),
