d5e72830f4e55cc189337c31b191c41b873798de
Angelika Prass <angelika.prass@op.pl>
2023-06-22 23:57:14 +0200

    prettier

--- src/lib/ui/footer/Regulations.svelte
+++ src/lib/ui/footer/Regulations.svelte
@@ -5,34 +5,25 @@
 <div class="page">
 	<SubpageH1>Regulamin serwisu włosiara.pl</SubpageH1>
 	<ol>
-
 		<li>
-			
 			Postanowienia ogólne:
 			<ul>
 				<li>
 					a. Niniejszy regulamin określa zasady korzystania z Serwisu Internetowego "wlosiara.pl".
 				</li>
-				<li>
-					b. Korzystając z Serwisu, Użytkownik akceptuje niniejszy regulamin.
-				</li>
-				<li>
-					c. Termin "Serwis" odnosi się do strony internetowej "wlosiara.pl".
-				</li>
+				<li>b. Korzystając z Serwisu, Użytkownik akceptuje niniejszy regulamin.</li>
+				<li>c. Termin "Serwis" odnosi się do strony internetowej "wlosiara.pl".</li>
 			</ul>
 		</li>
-		
+
 		<li>
 			Rejestracja i konto użytkownika:
 			<ul>
+				<li>a. Aby korzystać z pełnej funkcjonalności Serwisu, Użytkownik musi założyć konto.</li>
+				<li>b. Rejestracja konta wymaga podania prawdziwych, aktualnych danych.</li>
 				<li>
-					a. Aby korzystać z pełnej funkcjonalności Serwisu, Użytkownik musi założyć konto.
-				</li>
-				<li>
-					b. Rejestracja konta wymaga podania prawdziwych, aktualnych danych.
-				</li>
-				<li>
-					c. Użytkownik jest odpowiedzialny za zachowanie poufności swojego hasła i informacji logowania.
+					c. Użytkownik jest odpowiedzialny za zachowanie poufności swojego hasła i informacji
+					logowania.
 				</li>
 			</ul>
 		</li>
@@ -40,10 +31,12 @@
 			Ochrona danych osobowych:
 			<ul>
 				<li>
-					a. Serwis gwarantuje ochronę prywatności i bezpieczeństwo danych osobowych Użytkowników zgodnie z obowiązującymi przepisami prawa.
+					a. Serwis gwarantuje ochronę prywatności i bezpieczeństwo danych osobowych Użytkowników
+					zgodnie z obowiązującymi przepisami prawa.
 				</li>
 				<li>
-					b. Szczegółowe informacje na temat przetwarzania danych osobowych znajdują się w Polityce Prywatności Serwisu.
+					b. Szczegółowe informacje na temat przetwarzania danych osobowych znajdują się w Polityce
+					Prywatności Serwisu.
 				</li>
 			</ul>
 		</li>
@@ -52,10 +45,12 @@
 			Odpowiedzialność:
 			<ul>
 				<li>
-					a. Serwis nie ponosi odpowiedzialności za treści, informacje lub produkty dodawane przez Użytkowników.
+					a. Serwis nie ponosi odpowiedzialności za treści, informacje lub produkty dodawane przez
+					Użytkowników.
 				</li>
 				<li>
-					b. Użytkownik ponosi pełną odpowiedzialność za wszelkie szkody wynikające z naruszenia postanowień regulaminu lub używania Serwisu w sposób niezgodny z prawem.
+					b. Użytkownik ponosi pełną odpowiedzialność za wszelkie szkody wynikające z naruszenia
+					postanowień regulaminu lub używania Serwisu w sposób niezgodny z prawem.
 				</li>
 			</ul>
 		</li>
@@ -64,13 +59,13 @@
 			Zmiany w regulaminie:
 			<ul>
 				<li>
-					a. Serwis zastrzega sobie prawo do wprowadzania zmian w niniejszym regulaminie w dowolnym czasie.
+					a. Serwis zastrzega sobie prawo do wprowadzania zmian w niniejszym regulaminie w dowolnym
+					czasie.
 				</li>
+				<li>b. Zmiany w regulaminie będą publikowane na stronie internetowej Serwisu.</li>
 				<li>
-					b. Zmiany w regulaminie będą publikowane na stronie internetowej Serwisu.
-				</li>
-				<li>
-					c. Kontynuowanie korzystania z Serwisu po wprowadzeniu zmian oznacza akceptację nowych postanowień.
+					c. Kontynuowanie korzystania z Serwisu po wprowadzeniu zmian oznacza akceptację nowych
+					postanowień.
 				</li>
 			</ul>
 		</li>
@@ -79,15 +74,17 @@
 			Postanowienia końcowe:
 			<ul>
 				<li>
-					a. Wszelkie spory wynikające z korzystania z Serwisu będą rozstrzygane zgodnie z przepisami prawa właściwego dla siedziby Serwisu.
+					a. Wszelkie spory wynikające z korzystania z Serwisu będą rozstrzygane zgodnie z
+					przepisami prawa właściwego dla siedziby Serwisu.
 				</li>
 				<li>
-					b. W przypadku stwierdzenia nieważności lub niewykonalności któregokolwiek postanowienia niniejszego regulaminu, pozostałe postanowienia pozostają w mocy.
+					b. W przypadku stwierdzenia nieważności lub niewykonalności któregokolwiek postanowienia
+					niniejszego regulaminu, pozostałe postanowienia pozostają w mocy.
 				</li>
 			</ul>
 		</li>
 	</ol>
-Niniejszy regulamin wchodzi w życie z chwilą rozpoczęcia korzystania z Serwisu.
+	Niniejszy regulamin wchodzi w życie z chwilą rozpoczęcia korzystania z Serwisu.
 </div>
 
 <style>
