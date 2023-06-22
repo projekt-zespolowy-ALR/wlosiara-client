e9b2d9bed93fed4a4a4380be483a3981fa2f4916
Angelika Prass <angelika.prass@op.pl>
2023-06-22 02:01:18 +0200

    work in progress

--- src/lib/features/quiz/QuizResults.svelte
+++ src/lib/features/quiz/QuizResults.svelte
@@ -1,7 +1,25 @@
 <script lang="ts">
+	import type { SubmitFunction } from "@sveltejs/kit";
 	import type {DeepReadonly} from "ts-essentials";
-
+	import type { LayoutServerLoad } from "../../../routes/$types.js";
+	import { enhance } from "$app/forms";
 	export let userAnswerKindsCounter: DeepReadonly<Map<string, number>>;
+
+	const submit: SubmitFunction = async ({formElement}) => {
+		return ({result}) => {
+			console.log(result);
+			if (result.type === "success") {
+				formElement.reset();
+				alert("Wynik quizu został zapisany");
+			} else if (result.type === "error") {
+				console.log(result.error);
+			} else if (result.type === "failure") {
+				alert("Coś poszło nie tak");
+			} else {
+				console.log(result);
+			}
+		};
+	};
 
 	const maxType = (() => {
 		const entriesArray = Array.from(userAnswerKindsCounter.entries());
@@ -20,6 +38,19 @@
 			}
 		).kind;
 	})();
+	const load: LayoutServerLoad = async ({cookies}) => {
+	const [
+		{authService},
+		{
+			appConfig: {SESSION_TOKEN_COOKIE_NAME},
+		},
+	] = await Promise.all([
+		import("$lib/server/instances/authService.js"),
+		import("$lib/server/app_config/appConfig.js"),
+	]);
+	const sessionToken = cookies.get("session_token");
+	const session = sessionToken === undefined ? null : await authService.me(sessionToken);
+
 </script>
 
 <div class="answer">
@@ -27,6 +58,12 @@
 		<p>Brak odpowiedzi</p>
 	{:else}
 		<p>Twój typ włosów to: <span>{maxType}</span></p>
+		{#if session}
+		<form method="PUT" action="/send-result" use:enhance={submit}>
+			czy chcesz zapisać wynik quizu? 
+					<button> zapisz wynik </button>
+		</form>
+		{/if}
 	{/if}
 </div>
 
@@ -47,5 +84,15 @@
 	.answer span {
 		font-weight: bold;
 		color: var(--primary-color-3);
+	}
+
+	button {
+		background-color: var(--primary-color-3);
+		border: none;
+		border-radius: 5px;
+		color: var(--gray-8);
+		padding: 5px 10px;
+		margin: 10px 0;
+		cursor: pointer;
 	}
 </style>
